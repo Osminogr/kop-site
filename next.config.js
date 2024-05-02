a0ee9dfe24b79831458ca46a799bf81383432aca
Osminogr <56167624+Osminogr@users.noreply.github.com>
2024-05-02 06:07:42 +0000

    Deploying to gh-pages from @ Osminogr/kop-site@221fb8ff6beded3531f525c7c25e124df5290234 🚀

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/kop-site",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-  };
-
-module.exports = nextConfig
