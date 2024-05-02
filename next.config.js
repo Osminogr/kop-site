68e609e10ac7685e340293a9dc2c82f37bab27fd
Michail <osminogr@yandex.ru>
2024-05-02 10:45:46 +0700

    fc_1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: "/kop-site",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
 
 module.exports = nextConfig
